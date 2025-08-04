15ddb9b03ad219ae45058d63220c4309bca2aa7f
David <david@sulaco.co.uk>
2025-08-04 20:43:59 +0100

    production start backend script

--- backend/bin/start_container.sh
+++ backend/bin/start_container.sh
@@ -3,5 +3,5 @@
 if [ "$ENVIRONMENT" == "development" ] ; then
   uvicorn main:app --host 0.0.0.0:8000 --reload --log-level debug
 else
-  uvicorn main:app --host 0.0.0.0:8000 --log-level warning
+  uvicorn main:app --log-level=debug
 fi
