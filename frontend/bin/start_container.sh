60f1c4f93affbc0acc5723b51516d5c58fdf1011
David <david@sulaco.co.uk>
2025-08-04 16:16:53 +0100

    initial frontend setup

--- frontend/bin/start_container.sh
+++ frontend/bin/start_container.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-sleep infinity
+echo VITE_GOOGLE_OAUTH2_CLIENT_ID="$GOOGLE_OAUTH2_CLIENT_ID" > .env
+
+if [ "$ENVIRONMENT" = "development" ] ; then
+  npm run dev
+else
+  cd /opt/treadmilltracker_frontend_public/ && cp -r ../treadmilltracker.zz50.co.uk/dist .
+  sleep infinity
+fi
