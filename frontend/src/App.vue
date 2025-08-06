096884173428f46c54ff64b65b5f0e55a4b832c1
David <david@sulaco.co.uk>
2025-08-07 00:19:05 +0100

    eslint

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+x = 1
 </script>
 
 <template>
   <Header />
-  <div class="min-h-[800px] p-2 bg-white flex">
-  </div>
+  <div class="min-h-[800px] p-2 bg-white flex"></div>
   <Footer />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
