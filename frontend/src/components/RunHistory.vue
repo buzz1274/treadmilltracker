32934ebd4fb7102324e9b731bbfda7336e81f20c
David <david@sulaco.co.uk>
2025-08-20 01:09:26 +0100

    wireframes

--- frontend/src/components/RunHistory.vue
+++ frontend/src/components/RunHistory.vue
@@ -1,38 +1,67 @@
 <script setup lang="ts">
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import { PencilIcon, TrashIcon, PlusIcon } from '@heroicons/vue/24/solid'
 
 const runs = [
-  { date: '2025-08-01', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
-  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120' },
+  { date: '2025-08-01', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
+  { date: '2025-08-02', distance: '5km', time: '1:30', calories: '120', vo2: '39' },
 ]
 </script>
 
 <template>
   <DataTable
     :value="runs"
-    stripedRows
+    rows="10"
+    unstyled
+    paginator
+    paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+    :currentPageReportTemplate="'Showing {first} to {last} of {totalRecords}'"
     :pt="{
-      table: { class: 'text-xs px-2' },
-      headerRow: { class: 'h-7' },
-      bodyRow: { class: 'h-5 text-xs' },
+      tableContainer: 'w-100 pt-1 pl-2',
+      table: 'w-95',
+      header: 'bg-black text-white pl-2 ',
+      bodyRow: 'leading-loose text-xs hover:bg-surface-100 hover:pointer',
+      column: {
+        headerCell: 'text-left border-b border-black pb-2',
+        bodyCell: 'border-b border-gray-400 pt-1 pb-1',
+      },
+      pcPaginator: {
+        root: 'flex items-center justify-center pt-3',
+      },
     }"
   >
     <template #header>
-      <div class="font-semibold h-3 text-sm">Run History</div>
+      <div class="flex p-1 text-sm justify-between">
+        <div class="font-semibold">Run History</div>
+        <div class="pt-1 pr-4">
+          <PlusIcon @click="selectRow(data)" class="size-4" />
+        </div>
+      </div>
     </template>
 
-    <Column field="date" header="Date" class="border-gray:50"></Column>
+    <Column field="date" header="Date"></Column>
     <Column field="distance" header="Distance"></Column>
     <Column field="calories" header="Calories"></Column>
     <Column field="time" header="Time"></Column>
+    <Column class="w-10 !text-end">
+      <template #header>
+        <div class="text-center">-</div>
+      </template>
+      <template #body="{ data }">
+        <div class="flex items-center space-x-1">
+          <span @click="selectRow(data)"><PencilIcon class="size-4" /></span>
+          <span @click="selectRow(data)"><TrashIcon class="size-4" /></span>
+        </div>
+      </template>
+    </Column>
   </DataTable>
 </template>
--- frontend/src/views/RunsView.vue
+++ frontend/src/views/RunsView.vue
@@ -6,7 +6,6 @@ import RunHistory from '../components/RunHistory.vue'
   <div class="grid grid-cols-10 gap-4 w-screen">
     <div class="col-span-3">
       <RunHistory />
-      <RunHistory />
     </div>
     <div class="col-span-7">
       <!-- Content for the second column (70% width) -->
