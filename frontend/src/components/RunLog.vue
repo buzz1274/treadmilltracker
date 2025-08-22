66b5e9a2cfcf4c104e182a8f5d56faa8823ffa9c
David <david@sulaco.co.uk>
2025-08-22 17:53:01 +0100

    delete run log component

--- frontend/src/components/RunLog.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import BaseComponentHeader from '@/components/base/BaseComponentHeader.vue'
-</script>
-
-<template>
-  <BaseComponentHeader table-title="Run Log" />
-</template>
