fbe9998d3004399c69aea162fe35d6f5c265fbd9
David <david@sulaco.co.uk>
2025-09-25 15:30:58 +0100

    move main.py into app folder

--- backend/bin/start_container.sh
+++ backend/bin/start_container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$ENVIRONMENT" == "development" ] ; then
-  uvicorn main:app --host=0.0.0.0 --port=8001 --reload --log-level=debug
+  uvicorn app.main:app --host=0.0.0.0 --port=8001 --reload --log-level=debug
 else
-  uvicorn main:app --host=0.0.0.0 --port=8001 --log-level=warning
+  uvicorn app.main:app --host=0.0.0.0 --port=8001 --log-level=warning
 fi
